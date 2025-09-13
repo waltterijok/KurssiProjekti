1ec8d26e5b32cc03b50ebcb9a287a0f85dde1780
Waltteri Jokinen <Waltteri.Jokinen@student.lab.fi>
2025-09-13 15:43:25 +0300

    tehtävät 1-4

---
+++ A1_T4.py
@@ -0,0 +1,12 @@
+num1 = 47
+num2 = 102
+sum = 149
+diff = 55
+product = sum * diff
+print(f"{num1} + {num2} = {sum}")
+print(f"{num2} - {num1} = {diff}")
+print(f"{sum} * {diff} = {product}")
+print(f"({num1} + {num2}) * ({num2} - {num1}) = {product}")
+
+
+
